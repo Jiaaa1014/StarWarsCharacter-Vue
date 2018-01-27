7db0fbfcba86849f5d7a7378bb38c9dd46d53ef7
Jiaaa1014 <linao264590000@gmail.com>
2018-01-27 21:45:04 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h2>{{title}}</h2>
+    <h6 style="color:#666;">{{subtitle}}</h6>
+    <div class="char-box">
+      <Character
+      v-for="(id, key) in init_ids"
+      :id="id"
+      :key="key"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Character from "./components/Character.vue";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      title: "Who Do U Want to Keep in Your Team?",
+      subtitle: "Click the box and randomly replaced with another guy ",
+      init_ids: Array(5)
+        .fill()
+        .map(e => Math.floor(Math.random() * 87) + 1)
+    };
+  },
+  components: {
+    Character
+  }
+};
+</script>
